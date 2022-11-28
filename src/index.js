d09c4c10941f0809a32ad160f5f1255af010fe41
irsalsss <muhamadirsal@gmail.com>
2022-11-28 09:36:38 +0700

    back to app1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App1 from './App1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <App1 /> */}
+    {/* <App /> */}
+    <App1 />
   </React.StrictMode>
 );
